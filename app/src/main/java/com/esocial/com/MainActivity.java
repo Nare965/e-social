2ad2567306d28bd0473f9cde8a4246468e8134cb
Nare Asatryan <nare.asatryan18@gmail.com>
2025-04-06 02:18:32 +0400

    created login page and profile page.

--- app/src/main/java/com/esocial/com/MainActivity.java
+++ app/src/main/java/com/esocial/com/MainActivity.java
@@ -1,6 +1,11 @@
 package com.esocial.com;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.esocial.com.dao.DatabaseHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper;
+    private EditText editEmail, editPassword;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        dbHelper = new DatabaseHelper(this);
+
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        // Create a new user
+        if (!dbHelper.loginUser("john.doe@example.com", "123456")) {
+            dbHelper.insertUser("John", "Doe", "john.doe@example.com", "123456");
+        }
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editEmail.getText().toString().trim();
+                String password = editPassword.getText().toString();
+
+                if (dbHelper.loginUser(email, password)) {
+                    Toast.makeText(MainActivity.this, "Login done", Toast.LENGTH_SHORT).show();
+                    // Redirect to the profile page
+                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                    intent.putExtra("userEmail", email);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
---
+++ app/src/main/java/com/esocial/com/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.esocial.com;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("Login done ");
+
+    }
+}
